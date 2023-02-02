590e73a63cafa73937d5c1417855a95cdb9e4134
M.Donny <irwansyah.jr27@gmail.com>
2023-02-03 06:03:04 +0700

    initialize git

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        slate: colors.blueGray,
+      },
+    },
+  },
+  plugins: [],
+};
